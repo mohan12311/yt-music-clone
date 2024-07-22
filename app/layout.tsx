a644c74b1ac79b0262001153a39bde94f4c7a906
dh <dh0469@gmail.com>
2024-07-23 00:16:39 +0900

    Remove Unused Imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/themeProvider";
-import { sleep } from "@/lib/utils";
 import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,16 +19,14 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <ThemeProvider
+        <ThemeProvider
           attribute="class"
           defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
-      >
-          <Sidebar>
-              {children}
-          </Sidebar>
-      </ThemeProvider>
+        >
+          <Sidebar>{children}</Sidebar>
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/playlist/loading.tsx
+++ app/playlist/loading.tsx
@@ -1,10 +1,9 @@
-'use client'
+"use client";
 
-import {BarLoader} from "react-spinners";
 import LoadingBar from "@/components/LoadingBar";
 
 const loading = () => {
-    return <LoadingBar />
-}
+  return <LoadingBar />;
+};
 
-export default loading
+export default loading;
