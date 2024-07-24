e24010917f1c056455750eac8e83c58fc94676e2
dh <dh0469@gmail.com>
2024-07-25 00:06:48 +0900

    Refactor: UserIcon.tsx to reactive component

--- components/UserIcon.tsx
+++ components/UserIcon.tsx
@@ -1,9 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import React from "react";
+import { cn } from "@/lib/utils";
 
-const UserIcon = () => {
+const UserIcon = ({ size = "sm" }) => {
   return (
-    <Avatar>
+    <Avatar className={cn("w-6.5 h-6.5" && size === "lg" && "w-14 h-14")}>
       <AvatarImage src="https://github.com/shadcn.png" />
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>
